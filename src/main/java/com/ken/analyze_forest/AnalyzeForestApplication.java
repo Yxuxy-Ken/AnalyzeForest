0a3f1c9951b968ec078b9ca69624e5b0fa149c93
ken <yxuxy.ken@gmail.com>
2023-08-31 14:22:29 +0800

    init commit

---
+++ src/main/java/com/ken/analyze_forest/AnalyzeForestApplication.java
@@ -0,0 +1,13 @@
+package com.ken.analyze_forest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeForestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyzeForestApplication.class, args);
+    }
+
+}
